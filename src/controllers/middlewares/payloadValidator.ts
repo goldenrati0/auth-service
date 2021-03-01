53c92deb3043f5090529176441ca250b879a42ea
Tahmid Choyon <tahmid.hossain@newscred.com>
2021-03-01 07:05:01 +0600

    Modify param validator middleware

--- src/controllers/middlewares/payloadValidator.ts
+++ src/controllers/middlewares/payloadValidator.ts
@@ -2,22 +2,27 @@ import { NextFunction, Request, Response } from 'express';
 import createHttpError from 'http-errors';
 import Joi, { ValidationError } from 'joi';
 
-export const validateParams = (paramSchema: Joi.ObjectSchema<any>, params: any = null) => {
-  return (req: Request, res: Response, next: NextFunction) => {
-    const requestPayload = params ?? req.body;
-    try {
-      Joi.assert(requestPayload, paramSchema);
-      next();
-    } catch (error: unknown) {
-      if (error instanceof ValidationError) {
-        const errorString = error.details.map(err => `${err.message}`).join(', ');
-        throw new createHttpError.BadRequest(errorString);
-      }
-      throw new createHttpError.InternalServerError();
+export const assertPayloadAgainstSchema = (payload: any, schema: Joi.ObjectSchema<any>): void => {
+  try {
+    Joi.assert(payload, schema);
+  } catch (error: unknown) {
+    if (error instanceof ValidationError) {
+      const errorString = error.details.map(err => `${err.message}`).join(', ');
+      throw new createHttpError.BadRequest(errorString);
     }
+    throw new createHttpError.InternalServerError();
+  }
+};
+
+export const validateParams = (paramSchema: Joi.ObjectSchema<any>, payload: any = null) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    const requestPayload = payload ? payload : req.body;
+    assertPayloadAgainstSchema(requestPayload, paramSchema);
+    next();
   };
 };
 
 module.exports = {
   validateParams,
+  assertPayloadAgainstSchema,
 };
