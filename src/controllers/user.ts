ce29972533d859f943b0fcc5e5b65801948ce4b6
Tahmid Choyon <tahmid.hossain@newscred.com>
2021-03-01 07:08:06 +0600

    Add new user controllers

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,10 +1,12 @@
 import { Request, Response } from 'express';
-import { login, signup } from '../repository/user';
+import { login, create, getUserRoles, addRolesToUser, checkUserPermissions } from '../repository/user';
 import { signAccessTokenForUser } from './jwtUtilities/jwt';
+import { assertPayloadAgainstSchema } from './middlewares/payloadValidator';
+import { userIdInPathParamSchema } from './schemas/request.schema';
 
 export const userSignupController = async (req: Request, res: Response) => {
   const { username, email, password } = req.body;
-  const user = await signup({
+  const user = await create({
     username,
     email,
     password,
@@ -23,7 +25,35 @@ export const userLoginController = async (req: Request, res: Response) => {
   return res.json(token);
 };
 
+export const listUserRoles = async (req: Request, res: Response) => {
+  assertPayloadAgainstSchema(req.params, userIdInPathParamSchema);
+  const { userId } = req.params;
+  const roles = await getUserRoles(userId);
+  return res.json(roles);
+};
+
+export const addRoles = async (req: Request, res: Response) => {
+  assertPayloadAgainstSchema(req.params, userIdInPathParamSchema);
+  const { userId } = req.params;
+  const { roles } = req.body;
+
+  const user = await addRolesToUser({ userId, roles });
+  return res.json(user);
+};
+
+export const checkPermissions = async (req: Request, res: Response) => {
+  assertPayloadAgainstSchema(req.params, userIdInPathParamSchema);
+  const { userId } = req.params;
+  const { permissions } = req.body;
+
+  const allowedPermissions = await checkUserPermissions({ userId, permissions });
+  return res.json(allowedPermissions);
+};
+
 module.exports = {
   userSignupController,
   userLoginController,
+  listUserRoles,
+  addRoles,
+  checkPermissions
 };
