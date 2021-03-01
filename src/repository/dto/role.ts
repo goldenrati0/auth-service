3103cafc488a4625407b57bfed6fb54cc7e43c03
Tahmid Choyon <tahmid.hossain@newscred.com>
2021-03-01 07:06:25 +0600

    Add Role DTO and mapper

--- src/repository/dto/role.ts
+++ src/repository/dto/role.ts
@@ -1,6 +1,11 @@
 import PermissionDTO from './permission';
 
+export type RoleCreate = {
+  name: string;
+  permissions: string[];
+};
 export default interface RoleDTO {
+  id: string;
   name: string;
   permissions: PermissionDTO[];
 }
--- src/repository/mappers/role.ts
+++ src/repository/mappers/role.ts
@@ -1,15 +1,16 @@
 import RoleModel from '../../models/role.schema';
 import { IRole } from '../../models/interfaces/role.interface';
-import RoleDTO from '../dto/role';
+import RoleDTO, { RoleCreate } from '../dto/role';
 import PermissionMapper from './permission';
 
-const toDomain = ({ name, permissions }: any): IRole =>
+const toDomain = ({ name, permissions }: RoleCreate): IRole =>
   new RoleModel({
     name,
     permissions,
   });
 
-const toDTO = ({ name, permissions }: IRole): RoleDTO => ({
+const toDTO = ({ _id: id, name, permissions }: IRole): RoleDTO => ({
+  id,
   name,
   permissions: permissions.map(PermissionMapper.toDTO),
 });
